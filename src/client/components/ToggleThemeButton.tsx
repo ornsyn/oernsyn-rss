719aeb10107fee5bf3ff636fe3b39aaf4c387410
ornsyn <peter@ornsyn.no>
2025-03-31 21:11:45 +0200

    Update ToggleThemeButton.tsx

--- src/client/components/ToggleThemeButton.tsx
+++ src/client/components/ToggleThemeButton.tsx
@@ -7,7 +7,7 @@ import styles from "../../../styles/RefreshButton.module.css";
 const onRouteChangeStart = () => NProgress.start();
 const onRouteChangeDone = () => NProgress.done();
 
-export const ToggleThemeButton = () => {
+export const RefreshButton = () => {
   const router = useRouter();
   
   const toggleTheme = () => {
